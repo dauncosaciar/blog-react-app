b6e7fe36510ca7289846f9d3492b4c0b74efbe96
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-01-10 22:36:21 -0300

    Página de Error 404

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -1,13 +1,18 @@
 /* Página que muestra un mensaje de recurso no encontrado */
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Error404.scss";
 
 export default function Error404() {
   return (
     <div className="error404">
-      <h1>Error404...</h1>
+      <h1>Error 404</h1>
+      <h2>Página No Encontrada</h2>
+      <Link to="/">
+        <h3>Volver al inicio</h3>
+      </Link>
     </div>
   );
 }
